a5ce8c50fe9ba92d1006ff60a05e2d003aec38a2
rlzzj <2275045670@qq.com>
2022-06-13 22:57:41 +0800

    b

--- releaseAndoird.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\yf\Desktop\MindustryMods\Mindustry-CN-ARC
-
-.\gradlew android:assembleRelease -PRELEASE_STORE_FILE=C:/Users/yf/test.jks -PRELEASE_STORE_PASSWORD="wdsj1.6.2" -PRELEASE_KEY_ALIAS="test" -PRELEASE_KEY_PASSWORD="wdsj1.6.2"
