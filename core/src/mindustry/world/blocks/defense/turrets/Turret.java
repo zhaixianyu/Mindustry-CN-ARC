8fbb263d3765f286da0b75c4237df478128f96bc
blac8 <105403704+blac8@users.noreply.github.com>
2022-07-15 08:14:59 +0800

    显示炮塔充能进度

--- core/src/mindustry/world/blocks/defense/turrets/Turret.java
+++ core/src/mindustry/world/blocks/defense/turrets/Turret.java
@@ -7,6 +7,7 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.math.geom.*;
+import arc.scene.ui.layout.Table;
 import arc.struct.*;
 import arc.util.*;
 import arc.util.io.*;
@@ -695,6 +696,14 @@ public class Turret extends ReloadTurret{
         @Override
         public byte version(){
             return 1;
+        }
+
+        @Override
+        public void displayBars(Table bars) {
+            super.displayBars(bars);
+            if (minWarmup > 0f) {
+                bars.add(new Bar(() -> Core.bundle.format("stat.warmupDetail", (int)(shootWarmup * 100 / minWarmup)), () -> Pal.ammo, () -> shootWarmup / minWarmup)).row();
+            }
         }
     }
 
