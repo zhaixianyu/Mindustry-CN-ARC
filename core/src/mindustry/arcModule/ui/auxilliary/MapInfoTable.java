ee7f2852c9eaddfbe459bdb809a9266112a3d84d
Lucky Clover <jmj18795967847@gmail.com>
2022-08-31 00:40:19 +0800

    ui大全v2

--- core/src/mindustry/arcModule/ui/auxilliary/MapInfoTable.java
+++ core/src/mindustry/arcModule/ui/auxilliary/MapInfoTable.java
@@ -2,8 +2,11 @@ package mindustry.arcModule.ui.auxilliary;
 
 
 import arc.Core;
+import arc.graphics.Colors;
+import arc.scene.ui.Label;
 import arc.scene.ui.TextField;
 import arc.scene.ui.layout.*;
+import arc.struct.ObjectIntMap;
 import mindustry.content.*;
 import mindustry.editor.*;
 import mindustry.gen.*;
@@ -33,7 +36,6 @@ public class MapInfoTable extends BaseToolsTable{
 
         button(Icon.map, clearAccentNonei, mapInfoDialog::show).tooltip("地图信息");
         button(Items.copper.emoji(), clearLineNonet, this::floorStatisticDialog).tooltip("矿物信息");
-        button(Icon.starSmall, clearAccentNonei, ui.achievements::show).tooltip("统计与成就");
         button(Icon.chatSmall, clearAccentNonei, () -> ui.MessageDialog.show()).tooltip("中央监控室");
         button(Icon.playersSmall,clearAccentNonei,()->{
             if(ui.listfrag.players.size>1){
@@ -46,6 +48,7 @@ public class MapInfoTable extends BaseToolsTable{
                 ui.arcInfo("视角追踪：" + follow.name,3f);
             }
         }).tooltip("切换跟踪玩家");
+        button(Icon.starSmall, clearAccentNonei, ui.achievements::show).tooltip("统计与成就");
         button(Icon.editSmall,clearAccentNonei,this::uiTable).tooltip("ui大全");
     }
 
@@ -96,14 +99,45 @@ public class MapInfoTable extends BaseToolsTable{
         dialog.cont.row();
 
         dialog.cont.pane(c -> {
-            stringIcons.copy().each((name,iconc)->{
-                c.button(iconc,cleart, ()->{
-                    Core.app.setClipboardText(iconc);
-                    sField.setText(sField.getText() + iconc);
-                }).size(50f).tooltip(name);
-                uiRowIndex+=1;
-                if(uiRowIndex%15==0) c.row();
-            });
+            c.add("颜色").color(getThemeColor()).center().fillX().row();
+            c.image().color(getThemeColor()).fillX().row();
+            c.table(ct->{
+                Colors.getColors().forEach((colorEntry)->{
+                    ct.button("[#" + colorEntry.value  +"]" + colorEntry.key,cleart, ()->{
+                        Core.app.setClipboardText("[#" + colorEntry.value  +"]");
+                        sField.setText(sField.getText() + "[#" + colorEntry.value  +"]");
+                    }).size(50f).tooltip( colorEntry.key);
+                    uiRowIndex+=1;
+                    if(uiRowIndex%15==0) ct.row();
+                });
+            }).row();
+            c.add("物品").color(getThemeColor()).center().fillX().row();
+            c.image().color(getThemeColor()).fillX().row();
+            c.table(ct->{
+                uiRowIndex = 0;
+                stringIcons.copy().each((name,iconc)->{
+                    ct.button(iconc,cleart, ()->{
+                        Core.app.setClipboardText(iconc);
+                        sField.setText(sField.getText() + iconc);
+                    }).size(50f).tooltip(name);
+                    uiRowIndex+=1;
+                    if(uiRowIndex%15==0) ct.row();
+                });
+            }).row();
+            c.add("图标").color(getThemeColor()).center().fillX().row();
+            c.image().color(getThemeColor()).fillX().row();
+            c.table(ct->{
+                uiRowIndex = 0;
+                for(int i=0;i<Iconc.all.length();i++){
+                    String subString = Iconc.all.substring(i,i+1);
+                    ct.button(subString,cleart, ()->{
+                        Core.app.setClipboardText(subString);
+                        sField.setText(sField.getText() + subString);
+                    }).size(50f).tooltip(subString);
+                    uiRowIndex+=1;
+                    if(uiRowIndex%15==0) ct.row();
+                }
+            }).row();
         }).row();
 
         dialog.addCloseButton();
