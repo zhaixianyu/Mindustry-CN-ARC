2a7515cc4cf22e2ba94e1b81f254413a714668ac
Lucky Clover <jmj18795967847@gmail.com>
2022-11-14 19:53:12 +0800

    arc扫描模式完成！

--- core/src/mindustry/arcModule/toolpack/arcScanner.java
+++ core/src/mindustry/arcModule/toolpack/arcScanner.java
@@ -28,12 +28,12 @@ public class arcScanner {
     /**
      * 实际与画面的距离
      */
-    private static int ratio = 20;
+    private static float ratio = 10f;
 
     /**
      * 每多少范围一个雷达
      */
-    private static int radarCir = 25;
+    private static float radarCir = 25f;
     private static final int basicRadarCir = 25;
     /**
      * 需要多少s扫描完成，仅用于特效
@@ -53,62 +53,70 @@ public class arcScanner {
      * 实际扫描范围
      */
     private static float curScanRange = 0;
-    private static int worldSize = 0;
+    private static float worldSize = 0;
 
     private static final Table t = new Table(Styles.black3);
 
+    private static int expandRate = 1;
+
 
     static {
-            t.touchable = Touchable.disabled;
-            t.margin(8f).add(">> 扫描模式已启动 <<").color(getThemeColor()).style(Styles.outlineLabel).labelAlign(Align.center);
-            t.visible = true;
-            t.update(() -> t.setPosition(Core.graphics.getWidth()/2f, Core.graphics.getHeight() * 0.8f, Align.center));
-            t.pack();
-            t.act(0.1f);
-            Core.scene.add(t);
+        t.touchable = Touchable.disabled;
+        t.margin(8f).add(">> 扫描模式已启动 <<").color(getThemeColor()).style(Styles.outlineLabel).labelAlign(Align.center);
+        t.visible = true;
+        t.update(() -> t.setPosition(Core.graphics.getWidth() / 2f, Core.graphics.getHeight() * 0.8f, Align.center));
+        t.pack();
+        t.act(0.1f);
+        Core.scene.add(t);
     }
 
     public static void drawScanner() {
         if (Core.input.keyDown(Binding.arcDetail)) {
             Draw.reset();
             t.visible = true;
-            radarCir = (worldSize / basicRadarCir / 10 + 1) * basicRadarCir;
 
-            if (scanRate < 1f) scanRate += 1 / 60f / scanTime;
+            if (scanRate < 1f) scanRate = Math.min(scanRate + 1 / 60f / scanTime, 1f);
 
-            worldSize = (int)(Mathf.dst(world.width(), world.height()) / radarCir) * radarCir;
+            float playerToBorder = Math.max(Math.max(Math.max(Mathf.dst(player.tileX(), player.tileY()), Mathf.dst(world.width() - player.tileX(), player.tileY())), Mathf.dst(world.width() - player.tileX(), world.height() - player.tileY())), Mathf.dst(player.tileX(), world.height() - player.tileY()));
+            worldSize = Math.min(playerToBorder, (int) (Mathf.dst(world.width(), world.height()) / radarCir) * radarCir);
+            expandRate = (int) (worldSize / basicRadarCir / 10 + 1);
+            radarCir = expandRate * basicRadarCir;  //地图越大，radar间隔越大。此处选择最多10圈
             curScanRange = worldSize * tilesize * scanRate;
 
-            for (int i = 1; i < curScanRange / radarCir / tilesize; i++) {
+            for (int i = 1; i < curScanRange / radarCir / tilesize + 1; i++) {
                 Draw.color(player.team().color, 0.6f);
-                Lines.circle(player.x, player.y, (float) (radarCir * i * tilesize) / ratio);
-                arcDrawText(i * radarCir + "", 0.2f / Scl.scl(1f), player.x, player.y + i * radarCir * tilesize / ratio + 1f, getThemeColor(), 1);
+                Lines.circle(player.x, player.y, (radarCir * i * tilesize) / ratio);
+                arcDrawText(i * (int)radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x, player.y + i * radarCir * tilesize / ratio + 1f, getThemeColor(), 1);
             }
 
             if (scanRate < 1f) {
                 Draw.color(player.team().color, 0.8f);
                 Lines.circle(player.x, player.y, curScanRange / ratio);
             } else {
+                curScanRange = (int)(curScanRange / radarCir / tilesize + 1) * radarCir * tilesize;
+
                 Draw.color(player.team().color, 0.6f);
                 float curve = Mathf.curve(Time.time % 240f, 120f, 240f);
-                Lines.circle(player.x, player.y, curScanRange * Interp.pow3Out.apply(curve));
+                Lines.circle(player.x, player.y, curScanRange * Interp.pow3Out.apply(curve) / ratio);
+                Lines.rect(player.x - player.x / ratio, player.y - player.y / ratio, world.width() * tilesize / ratio, world.height() * tilesize / ratio);
             }
+
             Lines.line(player.x, player.y, player.x + curScanRange * Mathf.cos(Time.time * scanSpeed) / ratio, player.y + curScanRange * Mathf.sin(Time.time * scanSpeed) / ratio);
             Draw.reset();
 
             // 出怪点
             if (spawner.countSpawns() < 25 && !state.rules.pvp) {
                 for (Tile tile : spawner.getSpawns()) {
-                    Draw.color(state.rules.waveTeam.color,1f);
-                    arcDrawNearby(Icon.units.getRegion(), tile, 6, false, state.rules.waveTeam.color);
+                    Draw.color(state.rules.waveTeam.color, 1f);
+                    arcDrawNearby(Icon.units.getRegion(), tile, 6 * expandRate, false, state.rules.waveTeam.color);
                 }
             }
 
             for (Team team : Team.all) {
                 for (CoreBlock.CoreBuild core : team.cores()) {
                     if (state.rules.pvp && core.inFogTo(player.team())) continue;
-                    Draw.color(core.team.color, 0.8f);
-                    arcDrawNearby(core.block.fullIcon, core.tile, 4, true, core.team.color);
+                    Draw.color(core.team.color, 1f);
+                    arcDrawNearby(core.block.fullIcon, core.tile, 4 * expandRate, false, core.team.color);
                 }
             }
         } else {
@@ -130,7 +138,7 @@ public class arcScanner {
         float ny = player.y + Mathf.sin(rot) * range / ratio;
         if (rotate) Draw.rect(region, nx, ny, size, size, (float) Math.toDegrees(rot));
         else Draw.rect(region, nx, ny, size, size);
-        arcDrawText((int) (range / 8f) + "", 0.2f / Scl.scl(1f), nx, ny + size / 2, color, 1);
+        arcDrawText((int) (range / 8f) + "", 0.2f / Scl.scl(1f) * expandRate, nx, ny + size / 2, color, 1);
     }
 
 }
