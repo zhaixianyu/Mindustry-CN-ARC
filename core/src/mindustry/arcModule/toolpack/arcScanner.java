e26cdac296e6d8148b9ddfc7855c799997a71ca3
Lucky Clover <jmj18795967847@gmail.com>
2022-11-16 17:08:52 +0800

    小修雷达

--- core/src/mindustry/arcModule/toolpack/arcScanner.java
+++ core/src/mindustry/arcModule/toolpack/arcScanner.java
@@ -75,7 +75,7 @@ public class arcScanner {
         Core.scene.add(t);
 
         Events.on(EventType.WorldLoadEvent.class, event -> {
-            scanTime = Math.max(Mathf.dst(world.width(), world.height()) / 25f, 7.5f);
+            scanTime = Math.max(Mathf.dst(world.width(), world.height()) / 20f, 7.5f);
         });
     }
 
@@ -83,115 +83,115 @@ public class arcScanner {
         if (Core.input.keyDown(Binding.arcDetail)) {
             Draw.reset();
             t.visible = true;
-
             if (scanRate < 1f) scanRate = Math.min(scanRate + 1 / 60f / scanTime, 1f);
-
-            float playerToBorder = Math.max(Math.max(Math.max(Mathf.dst(player.tileX(), player.tileY()), Mathf.dst(world.width() - player.tileX(), player.tileY())), Mathf.dst(world.width() - player.tileX(), world.height() - player.tileY())), Mathf.dst(player.tileX(), world.height() - player.tileY()));
-            worldSize = Math.min(playerToBorder, (int) (Mathf.dst(world.width(), world.height()) / radarCir) * radarCir);
-            expandRate = (int) (worldSize / basicRadarCir / 10 + 1);
-            radarCir = expandRate * basicRadarCir;  //地图越大，radar间隔越大。此处选择最多10圈
-            curScanRange = worldSize * tilesize * scanRate;
-
-            for (int i = 1; i < curScanRange / radarCir / tilesize + 1; i++) {
-                Draw.color(player.team().color, 0.6f);
-                Lines.stroke(expandRate);
-                Lines.circle(player.x, player.y, (radarCir * i * tilesize) / ratio);
-                float rRatio = (radarCir * i * tilesize) / ratio + 2f;
-                arcDrawText(i * (int) radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x, player.y + rRatio, getThemeColor(), 1);
-                arcDrawText(i * (int) radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x + rRatio * Mathf.cos(Mathf.PI * 7 / 6), player.y + rRatio * Mathf.sin(Mathf.PI * 7 / 6), getThemeColor(), 1);
-                arcDrawText(i * (int) radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x + rRatio * Mathf.cos(Mathf.PI * 11 / 6), player.y + rRatio * Mathf.sin(Mathf.PI * 11 / 6), getThemeColor(), 1);
-            }
-
-            if (scanRate < 1f) {
-                Draw.color(player.team().color, 0.8f);
-                Lines.stroke(expandRate);
-                Lines.circle(player.x, player.y, curScanRange / ratio);
-                Draw.color(player.team().color, 0.1f);
-                Fill.circle(player.x, player.y, curScanRange / ratio);
-            } else {
-                curScanRange = (int) (curScanRange / radarCir / tilesize + 1) * radarCir * tilesize;
-
-                Draw.color(player.team().color, 0.1f);
-                Fill.circle(player.x, player.y, curScanRange / ratio);
-
-                Draw.color(player.team().color, 0.6f);
-                float curve = Mathf.curve(Time.time % 360f, 120f, 360f);
-                Lines.stroke(expandRate * 1.5f);
-                Lines.circle(player.x, player.y, curScanRange * Interp.pow3Out.apply(curve) / ratio);
-                Lines.stroke(expandRate * 1.5f);
-
-                Draw.color(player.team().color, 0.2f);
-                Fill.rect(player.x - player.x / ratio + world.width() * tilesize / ratio / 2, player.y - player.y / ratio + world.height() * tilesize / ratio / 2, world.width() * tilesize / ratio, world.height() * tilesize / ratio);
-                Draw.color(player.team().color, 0.85f);
-                Lines.rect(player.x - player.x / ratio, player.y - player.y / ratio, world.width() * tilesize / ratio, world.height() * tilesize / ratio);
-            }
-
-            Draw.color(player.team().color, 0.8f);
-            Lines.line(player.x, player.y, player.x + curScanRange * Mathf.cos(Time.time * scanSpeed) / ratio, player.y + curScanRange * Mathf.sin(Time.time * scanSpeed) / ratio);
-            Draw.reset();
-
-            // 出怪点
-            if (spawner.countSpawns() < 25 && !state.rules.pvp) {
-                for (Tile tile : spawner.getSpawns()) {
-                    if (scanRate < 1f && Mathf.dst(tile.worldx() - player.x,tile.worldy() - player.y) > curScanRange) continue;
-
-                    Draw.color(state.rules.waveTeam.color, 1f);
-                    arcDrawNearby(Icon.units.getRegion(), tile, Math.max(6 * expandRate, state.rules.dropZoneRadius / ratio / 2), state.rules.waveTeam.color);
-
-                    float curve = Mathf.curve(Time.time % 200f, 60f, 200f);
-                    Draw.color(state.rules.waveTeam.color, 1f);
-                    Lines.stroke(expandRate * 1f);
-                    Lines.circle(player.x + (tile.worldx() - player.x) / ratio, player.y + (tile.worldy() - player.y) / ratio, state.rules.dropZoneRadius * Interp.pow3Out.apply(curve) / ratio);
-                    Draw.color(state.rules.waveTeam.color, 0.5f);
-                    Lines.stroke(expandRate * 0.8f);
-                    Lines.dashCircle(player.x + (tile.worldx() - player.x) / ratio, player.y + (tile.worldy() - player.y) / ratio, state.rules.dropZoneRadius * Interp.pow3Out.apply(curve) / ratio);
-                }
-            }
-            //绘制核心
-            for (Team team : Team.all) {
-                for (CoreBlock.CoreBuild core : team.cores()) {
-                    if (scanRate < 1f && Mathf.dst(core.x - player.x,core.y - player.y) > curScanRange) continue;
-                    if (state.rules.pvp && core.inFogTo(player.team())) continue;
-                    Draw.color(core.team.color, 1f);
-                    arcDrawNearby(core.block.fullIcon, core.tile, 4 * expandRate, core.team.color);
-                }
-            }
-            //绘制单位
-            for (Unit unit : Groups.unit) {
-                if (scanRate < 1f && Mathf.dst(unit.x - player.x,unit.y - player.y) > curScanRange) continue;
-                Draw.color(unit.team.color, 0.5f);
-                Fill.circle(transX(unit.x), transY(unit.y), unit.hitSize * unitSize);
-            }
-            //绘制玩家
-            for (Player unit : Groups.player) {
-                if (scanRate < 1f && Mathf.dst(unit.x - player.x,unit.y - player.y) > curScanRange) continue;
-
-                Draw.color(unit.team().color, 0.9f);
-
-                float angle = (float) Math.atan2(unit.unit().aimY - unit.y, unit.unit().aimX - unit.x);
-                Fill.tri(player.x + (unit.x + Mathf.cos(angle) * playerSize - player.x) / ratio, player.y + (unit.y + Mathf.sin(angle) * playerSize - player.y) / ratio,
-                        player.x + (unit.x + Mathf.cos(angle + Mathf.PI * 2 / 3) * playerSize - player.x) / ratio, player.y + (unit.y + Mathf.sin(angle + Mathf.PI * 2 / 3) * playerSize - player.y) / ratio,
-                        player.x + (unit.x + Mathf.cos(angle + Mathf.PI * 4 / 3) * playerSize - player.x) / ratio, player.y + (unit.y + Mathf.sin(angle + Mathf.PI * 4 / 3) * playerSize - player.y) / ratio);
-            }
-            //绘制arc标记
-            if(Marker.markList.size>0) {
-                Marker.markList.each(a->{
-                    if((Time.time - a.time) > Marker.retainTime) return;
-                    Draw.color(a.markType.color);
-                    Lines.stroke(expandRate * (1 - (Time.time % 180 + 30) / 210));
-
-                    Lines.circle(transX(a.markPos.x), transY(a.markPos.y), markerSize / ratio * (Time.time % 180) / 180);
-                    Lines.stroke(expandRate);
-                    Lines.circle(transX(a.markPos.x), transY(a.markPos.y), markerSize / ratio);
-                    Lines.arc(transX(a.markPos.x), transY(a.markPos.y), (markerSize - expandRate) / ratio,1 - (Time.time - a.time)/Marker.retainTime);
-                    Draw.reset();
-                });
-            }
-
         } else {
             t.visible = false;
-            scanRate = 0;
+            if (scanRate > 0f) scanRate = Math.max(scanRate - 3 / 60f / scanTime, 0f);
         }
+        if(scanRate <= 0) return;
+
+        float playerToBorder = Math.max(Math.max(Math.max(Mathf.dst(player.tileX(), player.tileY()), Mathf.dst(world.width() - player.tileX(), player.tileY())), Mathf.dst(world.width() - player.tileX(), world.height() - player.tileY())), Mathf.dst(player.tileX(), world.height() - player.tileY()));
+        worldSize = Math.min(playerToBorder, (int) (Mathf.dst(world.width(), world.height()) / radarCir) * radarCir);
+        expandRate = (int) (worldSize / basicRadarCir / 10 + 1);
+        radarCir = expandRate * basicRadarCir;  //地图越大，radar间隔越大。此处选择最多10圈
+        curScanRange = worldSize * tilesize * scanRate;
+
+        for (int i = 1; i < curScanRange / radarCir / tilesize + 1; i++) {
+            Draw.color(player.team().color, 0.6f);
+            Lines.stroke(expandRate);
+            Lines.circle(player.x, player.y, (radarCir * i * tilesize) / ratio);
+            float rRatio = (radarCir * i * tilesize) / ratio + 2f;
+            arcDrawText(i * (int) radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x, player.y + rRatio, getThemeColor(), 1);
+            arcDrawText(i * (int) radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x + rRatio * Mathf.cos(Mathf.PI * 7 / 6), player.y + rRatio * Mathf.sin(Mathf.PI * 7 / 6), getThemeColor(), 1);
+            arcDrawText(i * (int) radarCir + "", 0.2f / Scl.scl(1f) * expandRate, player.x + rRatio * Mathf.cos(Mathf.PI * 11 / 6), player.y + rRatio * Mathf.sin(Mathf.PI * 11 / 6), getThemeColor(), 1);
+        }
+
+        if (scanRate < 1f) {
+            Draw.color(player.team().color, 0.8f);
+            Lines.stroke(expandRate);
+            Lines.circle(player.x, player.y, curScanRange / ratio);
+            Draw.color(player.team().color, 0.1f);
+            Fill.circle(player.x, player.y, curScanRange / ratio);
+        } else {
+            curScanRange = (int) (curScanRange / radarCir / tilesize + 1) * radarCir * tilesize;
+
+            Draw.color(player.team().color, 0.1f);
+            Fill.circle(player.x, player.y, curScanRange / ratio);
+
+            Draw.color(player.team().color, 0.6f);
+            float curve = Mathf.curve(Time.time % 360f, 120f, 360f);
+            Lines.stroke(expandRate * 1.5f);
+            Lines.circle(player.x, player.y, curScanRange * Interp.pow3Out.apply(curve) / ratio);
+            Lines.stroke(expandRate * 1.5f);
+
+            Draw.color(player.team().color, 0.05f);
+            Fill.rect(player.x - player.x / ratio + world.width() * tilesize / ratio / 2, player.y - player.y / ratio + world.height() * tilesize / ratio / 2, world.width() * tilesize / ratio, world.height() * tilesize / ratio);
+            Draw.color(player.team().color, 0.85f);
+            Lines.rect(player.x - player.x / ratio, player.y - player.y / ratio, world.width() * tilesize / ratio, world.height() * tilesize / ratio);
+        }
+
+        Draw.color(player.team().color, 0.8f);
+        Lines.line(player.x, player.y, player.x + curScanRange * Mathf.cos(Time.time * scanSpeed) / ratio, player.y + curScanRange * Mathf.sin(Time.time * scanSpeed) / ratio);
+        Draw.reset();
+
+        // 出怪点
+        if (spawner.countSpawns() < 25 && !state.rules.pvp) {
+            for (Tile tile : spawner.getSpawns()) {
+                if (scanRate < 1f && Mathf.dst(tile.worldx() - player.x,tile.worldy() - player.y) > curScanRange) continue;
+
+                Draw.color(state.rules.waveTeam.color, 1f);
+                arcDrawNearby(Icon.units.getRegion(), tile, Math.max(6 * expandRate, state.rules.dropZoneRadius / ratio / 2), state.rules.waveTeam.color);
+
+                float curve = Mathf.curve(Time.time % 200f, 60f, 200f);
+                Draw.color(state.rules.waveTeam.color, 1f);
+                Lines.stroke(expandRate * 1f);
+                Lines.circle(player.x + (tile.worldx() - player.x) / ratio, player.y + (tile.worldy() - player.y) / ratio, state.rules.dropZoneRadius * Interp.pow3Out.apply(curve) / ratio);
+                Draw.color(state.rules.waveTeam.color, 0.5f);
+                Lines.stroke(expandRate * 0.8f);
+                Lines.dashCircle(player.x + (tile.worldx() - player.x) / ratio, player.y + (tile.worldy() - player.y) / ratio, state.rules.dropZoneRadius * Interp.pow3Out.apply(curve) / ratio);
+            }
+        }
+        //绘制核心
+        for (Team team : Team.all) {
+            for (CoreBlock.CoreBuild core : team.cores()) {
+                if (scanRate < 1f && Mathf.dst(core.x - player.x,core.y - player.y) > curScanRange) continue;
+                if (state.rules.pvp && core.inFogTo(player.team())) continue;
+                Draw.color(core.team.color, 1f);
+                arcDrawNearby(core.block.fullIcon, core.tile, 4 * expandRate, core.team.color);
+            }
+        }
+        //绘制单位
+        for (Unit unit : Groups.unit) {
+            if (scanRate < 1f && Mathf.dst(unit.x - player.x,unit.y - player.y) > curScanRange) continue;
+            Draw.color(unit.team.color, 0.5f);
+            Fill.circle(transX(unit.x), transY(unit.y), unit.hitSize * unitSize);
+        }
+        //绘制玩家
+        for (Player unit : Groups.player) {
+            if (scanRate < 1f && Mathf.dst(unit.x - player.x,unit.y - player.y) > curScanRange) continue;
+
+            Draw.color(unit.team().color, 0.9f);
+
+            float angle = (float) Math.atan2(unit.unit().aimY - unit.y, unit.unit().aimX - unit.x);
+            Fill.tri(player.x + (unit.x + Mathf.cos(angle) * playerSize - player.x) / ratio, player.y + (unit.y + Mathf.sin(angle) * playerSize - player.y) / ratio,
+                    player.x + (unit.x + Mathf.cos(angle + Mathf.PI * 2 / 3) * playerSize - player.x) / ratio, player.y + (unit.y + Mathf.sin(angle + Mathf.PI * 2 / 3) * playerSize - player.y) / ratio,
+                    player.x + (unit.x + Mathf.cos(angle + Mathf.PI * 4 / 3) * playerSize - player.x) / ratio, player.y + (unit.y + Mathf.sin(angle + Mathf.PI * 4 / 3) * playerSize - player.y) / ratio);
+        }
+        //绘制arc标记
+        if(Marker.markList.size>0) {
+            Marker.markList.each(a->{
+                if((Time.time - a.time) > Marker.retainTime) return;
+                Draw.color(a.markType.color);
+                Lines.stroke(expandRate * (1 - (Time.time % 180 + 30) / 210));
+
+                Lines.circle(transX(a.markPos.x), transY(a.markPos.y), markerSize / ratio * (Time.time % 180) / 180);
+                Lines.stroke(expandRate);
+                Lines.circle(transX(a.markPos.x), transY(a.markPos.y), markerSize / ratio);
+                Lines.arc(transX(a.markPos.x), transY(a.markPos.y), (markerSize - expandRate) / ratio,1 - (Time.time - a.time)/Marker.retainTime);
+                Draw.reset();
+            });
+        }
+
     }
 
     /**
