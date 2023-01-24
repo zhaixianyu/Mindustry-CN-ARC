34267d07d25412e0153f18d62ed5472fd8ba0760
squi2rel <501410836@qq.com>
2023-01-24 18:01:32 +0800

    fix crash

--- core/src/mindustry/arcModule/toolpack/arcScanMode.java
+++ core/src/mindustry/arcModule/toolpack/arcScanMode.java
@@ -136,7 +136,7 @@ public class arcScanMode {
         totalEffHealth = 0;
         totalDps = 0;
         checkInit();
-        waveInfo thisWave = arcWave.get(Math.min(state.wave, arcWave.size - 1));
+        waveInfo thisWave = arcWave.get(Math.min(state.wave, Math.max(arcWave.size - 1, 0)));
         for (Tile tile : spawner.getSpawns()) {
             if (Mathf.dst(tile.worldx(), tile.worldy(), Core.input.mouseWorldX(), Core.input.mouseWorldY()) < state.rules.dropZoneRadius) {
                 float curve = Mathf.curve(Time.time % 240f, 120f, 240f);
