4bc0a08dcd927b3a80cb270b289a05e9767d9154
Lucky Clover <jmj18795967847@gmail.com>
2023-07-15 10:48:14 +0800

    比赛强制透雾（本地端）

--- core/src/mindustry/game/FogControl.java
+++ core/src/mindustry/game/FogControl.java
@@ -102,6 +102,7 @@ public final class FogControl implements CustomChunk{
 
     public boolean isDiscovered(Team team, int x, int y){
         if(!state.rules.staticFog || !state.rules.fog || team == null || team.isAI()) return true;
+        if(player.team().id == 255 && Core.settings.getBool("forceRemoveFog")) return true;
 
         var data = getDiscovered(team);
         if(data == null) return false;
@@ -115,6 +116,7 @@ public final class FogControl implements CustomChunk{
 
     public boolean isVisibleTile(Team team, int x, int y){
         if(!state.rules.fog|| team == null || team.isAI()) return true;
+        if(player.team().id == 255 && Core.settings.getBool("forceRemoveFog")) return true;
 
         var data = data(team);
         if(data == null) return false;
