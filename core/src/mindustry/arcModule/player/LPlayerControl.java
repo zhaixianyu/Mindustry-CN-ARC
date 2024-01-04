569077d713fd80d1ccad7403e74aa39ddb651066
Lucky Clover <jmj18795967847@gmail.com>
2024-01-04 18:08:32 +0800

    临时测试 临时测试

---
+++ core/src/mindustry/arcModule/player/LPlayerControl.java
@@ -0,0 +1,34 @@
+package mindustry.arcModule.player;
+
+import mindustry.logic.LUnitControl;
+/*
+public enum LPlayerControl {
+    idle,
+    stop,
+    move("x", "y"),
+    approach("x", "y", "radius"),
+    pathfind("x", "y"),
+    autoPathfind,
+    boost("enable"),
+    target("x", "y", "shoot"),
+    targetp("unit", "shoot"),
+    itemDrop("to", "amount"),
+    itemTake("from", "item", "amount"),
+    payDrop,
+    payTake("takeUnits"),
+    payEnter,
+    mine("x", "y"),
+    flag("value"),
+    build("x", "y", "block", "rotation", "config"),
+    getBlock("x", "y", "type", "building", "floor"),
+    within("x", "y", "radius", "result"),
+    unbind;
+
+    public final String[] params;
+    public static final mindustry.arcModule.player.LPlayerControl[] all = values();
+
+    LPlayerControl(String... params){
+        this.params = params;
+    }
+}
+*/
