c7ffadac88fb3378ab8f8130cd14676cc22e7583
squi2rel <501410836@qq.com>
2024-07-24 11:52:53 +0800

    fix

--- core/src/mindustry/core/Version.java
+++ core/src/mindustry/core/Version.java
@@ -49,6 +49,7 @@ public class Version{
         }else{
             arcBuild = Strings.canParseInt(map.get("build")) ? Integer.parseInt(map.get("build")) : -1;
         }
+        build = arcBuild;
         ARCVars.arcVersion = Version.arcBuild + "";
         ARCVars.arcVersionPrefix = "<ARC~" + Version.arcBuild + ">";
     }
