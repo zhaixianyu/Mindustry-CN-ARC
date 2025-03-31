845921a92ddc5ee7b4e1ad8a2dc31da59a2eee4b
zhaixianyu <1141927681@qq.com>
2025-03-31 14:42:26 +0800

    修复“一键填充”未点击时不生效 新增填充完后若物品栏已空自动寻找容器补给

--- core/src/mindustry/arcModule/player/AutoFill.java
+++ core/src/mindustry/arcModule/player/AutoFill.java
@@ -3,8 +3,12 @@ package mindustry.arcModule.player;
 import arc.Core;
 import arc.Events;
 import mindustry.game.EventType;
+import mindustry.gen.Call;
+import mindustry.type.Item;
+import mindustry.type.ItemStack;
+import mindustry.world.blocks.storage.StorageBlock;
 
-import static mindustry.Vars.player;
+import static mindustry.Vars.*;
 
 public class AutoFill {
     public final static AutoFill INSTANCE = new AutoFill();
@@ -15,8 +19,16 @@ public class AutoFill {
         Events.run(EventType.Trigger.update, () -> {
             long timeMillis = System.currentTimeMillis();
             if (timeMillis > lastRunTime + interval && Core.settings.getBool("autoFill")) {
+                ItemStack stack = player.unit().stack;
+                Item item = player.unit().hasItem() ? stack.item : null;
                 lastRunTime = timeMillis;
                 player.dropItems();
+
+                if (item != null && !player.unit().hasItem()){
+                    indexer.eachBlock(player.team(), player.x, player.y, itemTransferRange,
+                            (build)-> build.block instanceof StorageBlock && build.items.get(item) > 0,
+                            (build)-> Call.requestItem(player, build,item,player.unit().maxAccepted(item)));
+                }
             }
         });
     }
