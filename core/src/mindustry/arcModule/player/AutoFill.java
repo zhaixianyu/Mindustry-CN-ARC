091911b0c512de1ed2a564bfa9a9f22094076347
zhaixianyu <1141927681@qq.com>
2025-03-31 10:55:13 +0800

    将“一键填充”改为开关

--- core/src/mindustry/arcModule/player/AutoFill.java
+++ core/src/mindustry/arcModule/player/AutoFill.java
@@ -9,7 +9,7 @@ import static mindustry.Vars.player;
 public class AutoFill {
     public final static AutoFill INSTANCE = new AutoFill();
     private long lastRunTime = System.currentTimeMillis();
-    public long interval = 0;
+    public long interval = 500;
     private AutoFill(){
         Events.run(EventType.Trigger.update, () -> {
             long timeMillis = System.currentTimeMillis();
